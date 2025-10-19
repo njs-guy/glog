1c36cdb5cec30757f60ab705b7e7b001ba670cb7
njs-guy <shockeynj@gmail.com>
2025-10-19 18:42:35 -0400

    adjust test messages

--- test_scene.gd
+++ test_scene.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _ready() -> void:
-	Glog.debug("rng", "Debug message.")
-	Glog.info("shop", "Info message.")
-	Glog.warn("flags", "Warn message.")
-	Glog.error("player", "Error message.")
+	Glog.debug("rng", "Rolled 50")
+	Glog.info("shop", "Player bought Holy Grail for 100G.")
+	Glog.warn("flags", "Grandma is about to explode.")
+	Glog.error("player", "Could not find PlayerController.")
